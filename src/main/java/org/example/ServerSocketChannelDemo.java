cd26d56d94e15c17e659e7851cc45c58b76159f6
小墩 <1921669890@qq.com>
2025-11-19 21:01:10 +0800

    初步功能实现

---
+++ src/main/java/org/example/ServerSocketChannelDemo.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class ServerSocketChannelDemo {
+        static String dest = "/home/desk/";
+//    static String dest = "E://";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("监听已经启动");
+
+        //端口号
+        int port = 8888;
+        //buffer
+
+        //ServerSocketChannel
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        //绑定
+        ssc.socket().bind(new InetSocketAddress(port));
+
+        //设置非阻塞模式
+        ssc.configureBlocking(false);
+
+        //监听有新链接传入
+        while (true) {
+//            System.out.println("Waiting for connections");
+            SocketChannel accept = ssc.accept();
+            if (accept == null) { //没有链接传入
+                Thread.sleep(2000);
+                continue;
+            }
+            System.out.println("Incoming connection from: " + accept.socket().getRemoteSocketAddress());
+//            先接受文件名字
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            int len;
+            len = accept.read(buf);
+            String fileName = new String(buf.array(), 0, len, StandardCharsets.UTF_8);
+            fileName = dest + fileName;
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+//            向client回复收到
+            accept.write(ByteBuffer.wrap("OK".getBytes()));
+
+//            写入数据
+            FileOutputStream fos = new FileOutputStream(file);
+            while ((len = accept.read(buf)) != -1) {
+                buf.flip();
+                fos.write(buf.array(), 0, len);
+            }
+
+        }
+    }
+}
---
+++ src/main/java/org/example/SocketChannelDemo.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+public class SocketChannelDemo {
+    //    static   String name = "E:\\project\\3\\Quick-File-Transfer\\src\\main\\java\\org\\example\\openjdk-8u43-linux-x64.tar.gz";
+    static String name = "E:\\project\\3\\Quick-File-Transfer\\target\\Quick-File-Transfer-1.0-SNAPSHOT.jar";
+//    static String name = "E:\\project\\3\\Quick-File-Transfer\\src\\main\\java\\org\\example\\test";
+
+    public static void main(String[] args) throws Exception {
+        //创建SocketChannel
+//        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 8888));
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("192.168.61.20", 8888));
+        //设置阻塞和非阻塞
+        socketChannel.configureBlocking(false);
+        //读操作
+        RandomAccessFile file = new RandomAccessFile(name, "rw");
+        FileChannel channel = file.getChannel();
+        String[] split = name.split("\\\\");
+        ByteBuffer buf = ByteBuffer.wrap(split[split.length - 1].getBytes());
+        socketChannel.write(buf);
+        buf = ByteBuffer.allocate(1024);
+        while (true){
+            int read = socketChannel.read(buf);
+            if (read>0) break;
+            else Thread.sleep(50);
+        }
+        long fileSize = file.length();
+        long transferred = 0;
+        int flag;
+        while ((flag = channel.read(buf)) != -1) {
+            buf.flip();
+            socketChannel.write(buf);
+            buf.clear();
+            transferred += flag;
+            int progress = (int) ((transferred * 100) / fileSize);
+            System.out.print("\r传输进度: " + progress + "% (" + transferred + "/" + fileSize + " bytes)");
+            Thread.sleep(1000);
+        }
+
+        socketChannel.close();
+        System.out.println("transfer over");
+    }
+}
